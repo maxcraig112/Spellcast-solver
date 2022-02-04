e964b47c3d08f8532191f669e46b6329e92ea527
Jumpingeal <maxcraig1@icloud.com>
2022-02-05 00:54:31 +1100

    added scoring system with ranking, BUG: GYRATES, DOESNT CONSIDER 2nd CHARACTER AFTER HALFWAY THROUGH WORD RECURSIVE FUNCTION

--- spellcast_main.py
+++ spellcast_main.py
@@ -2,12 +2,16 @@ from word_list import *
 
 class spellcast:
     
-    def __init__(self,chars: str, gems: str, letter_bonus: tuple, word_bonus: tuple,round: int = 1) -> None:
+    def __init__(self,chars: str, gems: str, letter_bonus: tuple,letter_multiplier: int, word_bonus: tuple = None,round: int = 1)-> None:
+        assert len(chars) == 25, "length of chars string must be 25"
+        assert len(gems) == 25, "length of gems string must be 25"
         self.SCORE = [1,4,5,3,1,5,3,4,1,7,6,3,4,2,1,4,8,2,2,2,4,5,5,7,4,8]
         self.chars = chars
         self.grid_chars = [[chars[5 * y + x] for x in range(5)] for y in range(5)]
         self.grid_gems = [[gems[5 * y + x] for x in range(5)] for y in range(5)]
-        self.letter_multiplier = 2 if round == 1 else 3
+        self.letter_bonus = letter_bonus
+        self.letter_multiplier = letter_multiplier
+        self.word_bonus = word_bonus
 
     def neighbours(self,coord_a,coord_b):
         return abs(coord_a[0]-coord_b[0]) <= 1 and abs(coord_a[1]-coord_b[1]) <= 1
@@ -60,15 +64,72 @@ class spellcast:
                     all_coords += [(y,x)]
         return all_coords
 
+    def get_word_score(self,word: list):
+        """
+        Input: a list of tuples representing the position of every character of the word on the board
+        Output: a integer value representing the score that that word would give you
+        """
+        score = 0
+        #for every character coord in list
+        if word == [(0, 1), (0, 0), (1, 1), (2, 0), (2, 1), (3, 2), (2, 3)]:
+            print('hello')
+        for coord in word:
+            score += self.SCORE[ord(self.grid_chars[coord[0]][coord[1]])-97] * (self.letter_multiplier if coord == self.letter_bonus else 1)
+        if self.word_bonus is not None and self.word_bonus in word:
+            score *= 2
+        if len(word) >= 7:
+            score += 20
+        return score
+    
+    def get_best_words(self):
+        all_words = []
+        all_word_coords = []
+        all_scores = []
+        for i in range(25,0,-1):
+            #get all pruned words of one length list
+            words = prune_words(self.chars,f"processed_word_lists\english_words25x\{i}-len.txt")
+            #for every resulting word
+            for word in words:
+                word_coords = self.is_viable_word(word)
+                #for every possible word coordinate, calculate highest scoring version
+                max_score = None
+                max_index = None
+                
+                for i in range(len(word_coords)):
+                    #if valid word
+                    if word_coords[i] is not None and len(word_coords[i]) == len(word) and None not in word_coords[i]:
+                        score = self.get_word_score(word_coords[i])
+                        if max_score == None or score > max_score:
+                            max_score, max_index = score, i
+                if max_score is not None:
+                    all_words += [word]
+                    all_word_coords += [word_coords[max_index]]
+                    all_scores += [max_score]
+        all_scores, all_words, all_word_coords = zip(*sorted(zip(all_scores,all_words,all_word_coords),reverse=True))
+        for i in range(min(len(all_scores),20)):
+            print(f"#{i+1}: {all_words[i]}, {all_scores[i]}, {all_word_coords[i]}")
+                        
 if __name__ == "__main__":
-    s = spellcast("qiuyinfgaikeaaaboeimsdiva","1000010010001101010101100",(3,3),(3,1))
+    #s = spellcast("ogaotbyolttrsekiatwefmoeg","0110000110111001100001000",(4,0),2,(3,4))
+    #s.get_best_words()
+
+    words = prune_words("ogaotbyolttrsekiatwefmoeg",["gyrates"])
+    s = spellcast("ogaotbyolttrsekiatwefmoeg","0110000110111001100001000",(0,0),2,(0,0))
+    s.is_viable_word("gyrates")
+    print(words)
     # word = "cat"
     # print(s.is_viable_word(word))
     # print(word)
-    for i in range(25,0,-1):
-        words = prune_words(s.chars,f"processed_word_lists\english_words25x\{i}-len.txt")
-        for word in words:
-            x = s.is_viable_word(word)
-            if None not in x:
-                print(word, x)
+    # for i in range(25,0,-1):
+    #     words = prune_words(s.chars,f"processed_word_lists\english_words25x\{i}-len.txt")
+    #     for word in words:
+    #         word_coords = s.is_viable_word(word)
+    #         #for every possible word_coordinate
+    #         for i in range(len(word_coords):
+    #             if None not in coord:
+
+    #         if None not in x:
+    #             print(word, x, s.get_word_score(x[0]))
+
         #print(words)
+    #print(s.get_word_score([(3, 2), (3, 3), (2, 2)]))
