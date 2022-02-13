6c9ab7227c1a14826655a74666207187dd148a22
Jumpingeal <maxcraig1@icloud.com>
2022-02-13 21:41:22 +1100

    fixed recursive algorithm

--- spellcast_main.py
+++ spellcast_main.py
@@ -28,13 +28,13 @@ class spellcast:
         #for every instance of the starting word
         start_coords = self.get_coord(word[0])
         for coords in start_coords:
-            all_viable_words += [self.is_viable_word_aux(word[1:],[coords])]
+            all_viable_words += [self.is_viable_word_aux(word[1:],[coords],len(word))]
 
         if len(all_viable_words) == 0:
             return None
         return all_viable_words
 
-    def is_viable_word_aux(self, word: str, coords):
+    def is_viable_word_aux(self, word: str, coords,length: int):
         
         #if word length is 0, reached end of word
         if len(word) == 0:
@@ -44,9 +44,15 @@ class spellcast:
         #for every coordinate
         for new_coord in all_coords:
             #if both word coords are neighbours and coordinate isn't already in list
+            # if new_coord == (3,2):
+            #     print("hello")
+            #if both word positions are viable, and letter hasn't already been used
             if self.neighbours(new_coord,coords[-1]) and new_coord not in coords:
                 #create new recursive
-                return self.is_viable_word_aux(word[1:],coords + [new_coord])
+                x = self.is_viable_word_aux(word[1:],coords + [new_coord],length)
+                if x is not None and len(x) == length:
+                    return x
+        return None
 
 
     def get_coord(self, char: str) -> list:
@@ -71,8 +77,8 @@ class spellcast:
         """
         score = 0
         #for every character coord in list
-        if word == [(0, 1), (0, 0), (1, 1), (2, 0), (2, 1), (3, 2), (2, 3)]:
-            print('hello')
+        # if word == [(0, 1), (0, 0), (1, 1), (2, 0), (2, 1), (3, 2), (2, 3)]:
+        #     print('hello')
         for coord in word:
             score += self.SCORE[ord(self.grid_chars[coord[0]][coord[1]])-97] * (self.letter_multiplier if coord == self.letter_bonus else 1)
         if self.word_bonus is not None and self.word_bonus in word:
@@ -113,10 +119,12 @@ if __name__ == "__main__":
     #s = spellcast("ogaotbyolttrsekiatwefmoeg","0110000110111001100001000",(4,0),2,(3,4))
     #s.get_best_words()
 
-    words = prune_words("ogaotbyolttrsekiatwefmoeg",["gyrates"])
-    s = spellcast("ogaotbyolttrsekiatwefmoeg","0110000110111001100001000",(0,0),2,(0,0))
-    s.is_viable_word("gyrates")
-    print(words)
+    #words = prune_words("iljsmibthaexwiuigearaquyo","processed_word_lists\english_words25x.txt")
+    s = spellcast("ogaotbyolttrsekiatwefmoeg","0110000110111001100001000",(4,0),2,None)
+    #s.get_best_words()
+    x = s.is_viable_word("gyrates")
+    print(x)
+    #print(words)
     # word = "cat"
     # print(s.is_viable_word(word))
     # print(word)
@@ -125,11 +133,10 @@ if __name__ == "__main__":
     #     for word in words:
     #         word_coords = s.is_viable_word(word)
     #         #for every possible word_coordinate
-    #         for i in range(len(word_coords):
-    #             if None not in coord:
-
-    #         if None not in x:
-    #             print(word, x, s.get_word_score(x[0]))
+    #         for i in range(len(word_coords)):
+    #             if None not in word_coords:
+    #                 print(word, word_coords[i], s.get_word_score(word_coords[0]))
+                
 
         #print(words)
     #print(s.get_word_score([(3, 2), (3, 3), (2, 2)]))
